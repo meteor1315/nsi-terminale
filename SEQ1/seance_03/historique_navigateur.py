3cf3eeaf91060f47de375b343f65b69fbe7cb10c
Metphet1315 <160623037+Metphet1315@users.noreply.github.com>
2026-09-21 11:50:06 +0200

    Update historique_navigateur.py

--- SEQ1/seance_03/historique_navigateur.py
+++ SEQ1/seance_03/historique_navigateur.py
@@ -1,30 +1,44 @@
 from collections import deque 
 
-def reculer(historique):
-    if historique["back"]: 
-        # Ajouter la page actuelle au début de forward
-        
-        # Retirer la première page back 
-        # et le placer dans current 
-
-     return historique 
-
-
-""" 
-État avant relculer(historique): 
-
-{
+def creer_historique():
+    return {
     "back": deque(["url_D3", "url_C1", "url_A8", "url_A5"]),
     "current": "url_F",
     "forward": deque(["url_Z", "url_alpha1"])
-}
+    }
 
-État attendu après relculer(historique):
 
+
+
+
+
+def reculer(historique):
+    if historique["back"]:
+        historique["forward"].appendleft(historique["current"])
+        historique["current"] = historique["back"].popleft() 
+    
+    return historique 
+
+
+hist = creer_historique()
+
+print("Avant :", hist)
+print("Après :", reculer(hist))
+
+
+
+"""
+Avant :
 {
-    "back": deque(["url_C1", "url_A8", "url_A5"]),
-    "current": "url_D3",
-    "forward": deque(["url_F", "url_Z", "url_alpha1"])
+  "back": deque(["url_D3", "url_C1", "url_A8", "url_A5"]),
+  "current": "url_F",
+  "forward": deque(["url_Z", "url_alpha1"])
 }
 
+Après :
+{
+  "back": deque(["url_C1", "url_A8", "url_A5"]),
+  "current": "url_D3",
+  "forward": deque(["url_F", "url_Z", "url_alpha1"])
+}
 """
